a79fdd2ce6419ffbd496ed9e599851edf0799f8a
Clark Scheff <clark@cyngn.com>
2015-01-05 15:30:16 -0800

    Themes: Port to AOSPA [5/6]
    Adds sepolicy for themes dependent on build commit
    
    Change-Id: Ie25ce0fb49e873f4c810b759d65b33b5cc984398
    
    Conflicts:
    	configs/themes_common.mk
    	sepolicy/file_contexts
    	sepolicy/mediaserver.te
    	sepolicy/sepolicy.mk
    	sepolicy/service_contexts
    	sepolicy/system.te

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,5 +1,6 @@
 #
 # This policy configuration will be used by all products that
+<<<<<<< HEAD
 #
 
 BOARD_SEPOLICY_DIRS += \
@@ -11,3 +12,21 @@ BOARD_SEPOLICY_UNION += \
     system.te \
     service_contexts \
     file_contexts 
+=======
+# inherit from vendor
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/pa/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    app.te \
+    bootanim.te \
+    file.te \
+    file_contexts \
+    mediaserver.te \
+    seapp_contexts \
+    service_contexts \
+    system.te \
+    zygote.te
+>>>>>>> c314996... Themes: Port to AOSPA [5/6]
